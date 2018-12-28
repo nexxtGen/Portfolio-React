fd31d3ad0b4d37c78ed92f750f021eb1dbb00b11
Kamil Szczypior <kszczypior1@gmail.com>
2018-12-28 23:27:13 +0100

    Test web server

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+const divStyle = {
+    color: 'balck',
+    background: 'green'
+}
 const Home = () => (
-    <h1>Witaj w portfolio KamilSz</h1>
+    <div styles={divStyle}>
+        <h1>Witaj w portfolio KamilSz</h1>   
+        <h1>Witaj w portfolio KamilSz</h1>
+        <h2>Witaj w portfolio KamilSz Witaj w portfolio ortfolio KamilSz</h2>
+        <h1>Witaj w portfolio KamilSz</h1>   
+        <h1>Witaj w portfolio KamilSz</h1>
+        <h2>Witaj w portfolio KamilSz Witaj w portfolio KamilSz Witaj w portfo KamilSz</h2>
+        <h1>Witaj w portfolio KamilSz</h1>   
+        <h1>Witaj w portfolio KamilSz</h1>
+        <h2>Witaj w portfolio KamilSz Witaj w portfolio ortfolio KamilSz</h2>
+        <h1>Witaj w portfolio KamilSz</h1>   
+        <h1>Witaj w portfolio KamilSz</h1>
+        <h2>Witaj w portfolio KamilSz Witaj w portfolio KamilSz Witaj w portfo KamilSz</h2>       
+    </div>
 );
 
 export default Home;
