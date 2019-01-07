4c0a75006ba3f868dc12eeb7b21bd74670f6dea8
Kamil Szczypior <kszczypior1@gmail.com>
2019-01-07 20:59:37 +0100

    Create 2 columns in  personal section

--- src/Containers/personal-data/personal-data.js
+++ src/Containers/personal-data/personal-data.js
@@ -11,23 +11,56 @@ class PersonalData extends Component {
     render() {
         return (
             <div className={"container " + this.props.appWallpaper}>
-                <div className="app-container">
-                    <div className="welcome-in-portfolio">
-                        <p className="div1-home-p">informaje <span className="div1-home-span">o mnie</span></p>                                            
-                        <div className="field-personal-container animation-in">                        
-                            <div className="first-module-container">
-                                <div>
-                                    <img className="my-photo" src={myPhoto}></img>
-                                </div>
-                                <div className="kamil-data-container">
-                                    <div className="kamil-data">
-                                        <p>Name: Kamil</p>
-                                        <p>Surname: Sz</p>
-                                        <p>Profession: Web Developer</p>
-                                        <p>Target: Fullstack JS</p>
+                <div className="app-container direction-flex-column">
+                    <div className="row-personal-data">
+                        <div className="welcome-in-my-data">
+                            <p className="div1-home-p">informaje <span className="div1-home-span">o mnie</span></p>                                            
+                            <div className="field-personal-container animation-in">                        
+                                <div className="first-module-container">
+                                    <div>
+                                        <img className="my-photo" src={myPhoto}></img>
+                                    </div>
+                                    <div className="kamil-data-container">
+                                        <div className="kamil-data">
+                                            <p>Imię: Kamil</p>
+                                            <p>Nazwisko: Sz</p>
+                                            <p>Profesja: Web Developer</p>
+                                            <p>Cel: Fullstack JS</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                    <div className="row-personal-data">
+                        <div className="column-data-50">                            
+                            <div className="personal-second-row-first-column">   
+                                <div className="strong-weak-handler-container">
+                                    <button>Silne strony</button>
+                                    <button>Słabe strony</button>
+                                </div>
+                                <div className="strong-weak">
+                                    <div className="sr-fc-lc">
+                                        <p>Silne strony</p>
+                                    </div>
+                                    <div className="sr-fc-rc">
+                                        <p>Kreatywność</p>
+                                        <p>Duża samodzielność przy rozwiązywaniu problemów z kodem</p>
+                                        <p>Adaptacja</p>
+                                        <p>Odpowiedzialnosć za swoje działania</p>
+                                        <p>Ugodowość</p>
+                                        <p>Otwartość</p>
+                                        <p>Umiejętnosć przyznawania się do własnych błędów</p>
+                                        <p>Wysoka empatia</p>
+                                        <p>Ambicja</p>
+                                    </div>
+                                </div>                                                             
+                            </div>
+                        </div>
+                        <div className="column-data-50">
+                           <div className="personal-second-row-second-column">
+
+                           </div>
                         </div>
                     </div>
                 </div>
