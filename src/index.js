5112438356eef9ff255bb4e3503d00efab71dd54
Kamil Szczypior <kszczypior1@gmail.com>
2019-01-09 23:53:29 +0100

    Initial react-ga

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './Store';
 import routes from './routes';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-132218599-1');
+ReactGA.pageview('/');
 
 render(
     <Provider store={store}>
